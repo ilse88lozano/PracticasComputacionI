ff77fa83f8e21e3e92157a310dadf6831f6f6a2f
ilse88lozano <71842678+ilse88lozano@users.noreply.github.com>
2020-10-12 15:57:08 -0500

    Update Calculadora.cpp

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -1,1 +1,41 @@
+/*===========================================================
+ * AUTHOR: Ilse Lozano
+ * ilse.lofi@gmail.com
+ * 10-10-2020
+ ============================================================*/
+#include <iostream>
+using namespace std;
+int main() {
+    float num1 = 0;
+    float num2 = 0;
+    char symb='s';
+    float result = 0;
 
+    cout << "Este programa hace operaciones basicas entre dos numeros."<< endl;
+    cout <<"Primer numero:";
+    cin >> num1;
+    cout <<"Operacion:";
+    cin >> symb;
+    cout <<"Segundo numero:";
+    cin >> num2;
+    if (symb == '+'){
+        result=num1+num2;
+    }
+    if (symb == '-'){
+        result=num1-num2;
+    }
+    if (symb == '*'){
+        result=num1*num2;
+    }
+    if (symb == '/' && num2 ==0){
+        cout <<"Las divisiones entre cero no están definidas";
+        return 0;
+    }
+    if (symb == '/'){
+        result=num1/num2;
+    }
+    if (symb == '%'){
+        result= (int)num1%(int)num2;
+    }
+    cout << "Resultado: " <<num1 <<symb <<num2 <<"=" <<result <<endl;
+}
