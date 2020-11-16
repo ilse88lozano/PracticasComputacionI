bac021fba64f8f3fdfcb3e63b2c5efcab49ef0fd
ilse88lozano <71842678+ilse88lozano@users.noreply.github.com>
2020-11-16 10:13:49 -0600

    Update inventario.h

--- Proyecto/inventario.h
+++ Proyecto/inventario.h
@@ -1,1 +1,26 @@
+#ifndef PROYECTO_COMPU_INVENTARIO_H
+#define PROYECTO_COMPU_INVENTARIO_H
+#include<string>
+#include <vector>
 
+using namespace std;
+
+class inventario
+{
+private:
+    vector<string> articleVec;
+    vector<int> nArticles;
+    vector<float> Cost;
+    vector<float> Sell;
+public:
+    void AddArticle(vector<string> articleVec,string article);
+    void DelArticle(vector<string> articleVec, string article);
+    int setAmount(vector<int> nArticles,int amount);
+    void setCost(vector<float> Cost,float cost);
+    void setSell(vector<float> Sell,float costsell );
+    void printInv(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell);
+    float calcGain(vector<int> nArticles, vector<float> Cost, vector<float> Sell);
+};
+
+
+#endif //PROYECTO_COMPU_INVENTARIO_H
