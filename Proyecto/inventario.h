dd31c8b114dd56ef89cd7e990eba156de2adc65e
ilse88lozano <71842678+ilse88lozano@users.noreply.github.com>
2020-11-17 00:56:01 -0600

    Update inventario.h

--- Proyecto/inventario.h
+++ Proyecto/inventario.h
@@ -1,3 +1,4 @@
+
 #ifndef PROYECTO_COMPU_INVENTARIO_H
 #define PROYECTO_COMPU_INVENTARIO_H
 #include<string>
@@ -13,14 +14,14 @@ private:
     vector<float> Cost;
     vector<float> Sell;
 public:
-    vector<string> AddArticle(vector<string> articleVec,string article);
-    void DelArticles(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell, int index, int quant);
-    vector<int> setAmount(vector<int> nArticles,int amount);
-    void DelArticle(vector<int> nArticles, int index, int quant);
-    vector<float> setCost(vector<float> Cost,float cost);
-    vector<float> setSell(vector<float> Sell,float costsell );
-    void printInv(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell);
-    float calcGain(vector<int> nArticles, vector<float> Cost, vector<float> Sell);
+    void AddArticle(string art);
+    void DelArticles( int index, int quant);
+    void setAmount(int amount);
+    void DelArticle(int index, int quant);
+    void setCost(float costo);
+    void setSell(float costsell );
+    void printInv();
+    float calcGain();
 };
 
 
