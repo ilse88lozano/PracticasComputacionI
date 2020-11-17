d8cbf3505e2402c32fd9d34f8538003bbc8411b1
ilse88lozano <71842678+ilse88lozano@users.noreply.github.com>
2020-11-16 23:05:48 -0600

    Update inventario.cpp

--- Proyecto/inventario.cpp
+++ Proyecto/inventario.cpp
@@ -2,45 +2,58 @@
 #include<iostream>
 #include<vector>
 #include<string>
+
 using namespace std;
 
 //Agregar artículos
-void inventario::AddArticle(vector<string> articleVec,string article){
+vector<string> inventario::AddArticle(vector<string> articleVec,string article){
     articleVec.push_back(article);
+    return(articleVec);
 }
 
 //Borrar artículos
-void inventario::DelArticle(vector<string> articleVec, string article){
+void inventario::DelArticles(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell, int index,int quant){
 
+    articleVec.erase(articleVec.begin() + index );
+    nArticles.erase(nArticles.begin() + index );
+    Cost.erase(Cost.begin()+index);
+    Sell.erase(Sell.begin()+index);
+}
 
+void inventario::DelArticle(vector<int> nArticles, int index,int quant){
+
+    nArticles[index]-=nArticles[quant];
 }
 
 //Establece el numero de articulos para cada articlo
-void inventario::setAmount(vector<int> nArticles,int amount){
-    nArticles.pushback(amount);
+vector<int> inventario::setAmount(vector<int> nArticles,int amount){
+    nArticles.push_back(amount);
+    return(nArticles);
 }
 
 //Establece el costo de compra
-void inventario::setCost(vector<float> Cost,float cost){
+vector<float> inventario::setCost(vector<float> Cost,float cost){
 
     Cost.push_back(cost);
+    return(Cost);
 }
 //Establece el costo de venta
-void inventario::setSell(vector<float> Sell,float cost_sell ){
+vector<float> inventario::setSell(vector<float> Sell,float cost_sell ){
 
     Sell.push_back(cost_sell);
+    return(Sell);
 }
 
 
 //Tenemos los 4 vectores: articulo, cantidad, costo y  precio de venta
 void inventario::printInv(vector<string> articleVec,vector<int> nArticles,vector<float> Cost,vector<float> Sell){
     //Imprime los encabezados
-    cout<<"|"<<"Articulos"\t<<"No. Articulos"\t<<"Costo compra"\t<<"Costo venta"\t<<"|"<<endl;
+    cout<<"|"<<"Articulos"<<"\t"<<"No. Articulos"<<"\t"<<"Costo compra"<<"\t"<<"Costo venta"<<"\t"<<"|"<<endl;
     //Imprime una linea divisoria
-    cout<<"_______________________________________________________________________"<<endl;
+    cout<<"------------------------------------------------------------------"<<endl;
     //Imprime los elementos de los cuatro vectores de datos, en orden descendente para aparentar una tabla
     for (int i=0; i<articleVec.size();i++ ){
-        cout<<"|"<<articleVec[i]\t<<nArticles[i]\t<<Cost[i]\t<<Sell[i]\t<<"|"endl;
+        cout<<"|"<<i<<"\t"<<articleVec[i]<<"\t"<<nArticles[i]<<"\t"<<Cost[i]<<"\t"<<Sell[i]<<"\t"<<"|"<<endl;
     }
 }
 
@@ -53,4 +66,3 @@ float inventario::calcGain(vector<int> nArticles,vector<float> Cost,vector<float
         sumVent+=Sell[i]*nArticles[i];
     }
     return(sumVent-sumCost);
-}
