b5ea062c7a2c2479403c9d77fbe02864ba224212
ilse88lozano <71842678+ilse88lozano@users.noreply.github.com>
2020-11-27 19:44:35 -0600

    Update Grupo.h

--- PuntoIsoelectrico/Grupo.h
+++ PuntoIsoelectrico/Grupo.h
@@ -1,1 +1,25 @@
+#ifndef PUNTOISOELECTRICO_GRUPO_H
+#define PUNTOISOELECTRICO_GRUPO_H
 
+#include <string>
+using namespace std;
+
+class Grupo{
+private:
+    string nombreGrupo;
+    float pKa;
+    int carga;
+public:
+    Grupo() {
+        nombreGrupo = "";
+        pKa = 0;carga=0;
+    };
+    Grupo(string nom, float pk, int c) {
+        nombreGrupo = nom;
+        pKa = pk; carga=c;
+    }
+    int getCarga(float ph);
+    float getpKa();
+};
+
+#endif //PUNTOISOELECTRICO_GRUPO_H
