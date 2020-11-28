9efe48579016733434d4fdbe744d038f89670f8e
ilse88lozano <71842678+ilse88lozano@users.noreply.github.com>
2020-11-27 19:42:55 -0600

    Update Molecula.cpp

--- PuntoIsoelectrico/Molecula.cpp
+++ PuntoIsoelectrico/Molecula.cpp
@@ -1,1 +1,36 @@
+//
+// Created by usuario on 11/27/20.
+//
+#include "Molecula.h"
+
+void Molecula::AgregarGrupo(Grupo x){
+    grupos.push_back(x);
+    cantidadGrupos++;
+}
+int Molecula::cargaTotal(float ph){
+    int sum=0;
+    for (int j = 0; j < cantidadGrupos; ++j) {
+        sum+=grupos[j].getCarga(ph);
+    }
+    return sum;
+}
+float Molecula::CalcularPuntoIsoelectrico(){
+    int sum=-1000;
+    int c=0;
+    float limInf=0;
+    float limSup=0;
+    float ph=0;
+    for(int i=0;i<cantidadGrupos;i++){
+        ph=grupos[i].getpKa();
+        sum=cargaTotal(ph);
+        if(sum==0){
+            limSup=ph;
+        } else{
+            if (cargaTotal(ph+0.01)==0){
+                limInf=ph;
+            }
+        }
+    }
+    return (limInf+limSup)/2;
+}
 
