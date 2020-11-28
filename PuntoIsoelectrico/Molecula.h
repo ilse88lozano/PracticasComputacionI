a692991ffc6d8576462e2693b55eb0e4a26dbf44
ilse88lozano <71842678+ilse88lozano@users.noreply.github.com>
2020-11-27 19:43:42 -0600

    Update Molecula.h

--- PuntoIsoelectrico/Molecula.h
+++ PuntoIsoelectrico/Molecula.h
@@ -1,1 +1,24 @@
+#ifndef PUNTOISOELECTRICO_MOLECULA_H
+#define PUNTOISOELECTRICO_MOLECULA_H
+#include <string>
+#include <vector>
 
+#include "Grupo.h"
+
+using namespace std;
+class Molecula
+{
+public:
+    void AgregarGrupo(Grupo x);
+    float CalcularPuntoIsoelectrico();
+    int cargaTotal(float ph);
+    Molecula(string nombM) {
+        nombreMolecula = nombM;
+        cantidadGrupos=0;}
+private:
+    string nombreMolecula;
+    int cantidadGrupos;
+    vector<Grupo> grupos;
+};
+
+#endif //PUNTOISOELECTRICO_MOLECULA_H
