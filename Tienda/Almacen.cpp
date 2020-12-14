5a515235a822e5997caec3f0aacfa0e49c6b1a15
ilse88lozano <71842678+ilse88lozano@users.noreply.github.com>
2020-12-13 20:57:42 -0600

    Update Almacen.cpp

--- Tienda/Almacen.cpp
+++ Tienda/Almacen.cpp
@@ -1,1 +1,43 @@
+#include "Almacen.h"
 
+bool Almacen::ModificarInventarioArticulo(Articulo* artPtr, int cantidad)
+{
+    for (int i = 0; i < articulos.size(); ++i) {
+        if ((*artPtr).nombre==articulos[i].articulo->nombre){
+            if (cantidad+articulos[i].cantidad<0){
+                return false;
+            } else {
+                articulos[i].cantidad+=cantidad;
+                return true;
+            }
+        }
+    }
+    if (cantidad<=0){
+        return false;
+    } else {
+        ArticuloAlmacenado artic;
+        artic.articulo=artPtr;
+        artic.cantidad=cantidad;
+        articulos.push_back(artic);
+        return true;
+    }
+}
+
+string Almacen::ObtenerNombre() const
+{
+    return nombre;
+}
+
+string Almacen::ObtenerInventario() const
+{
+    string invStr = "";
+    invStr.append("\nArticulo\tCantidad\n");
+    for (int i = 0, n = articulos.size(); i < n; i++)
+    {
+        invStr.append(articulos[i].articulo->nombre);
+        invStr.append("\t");
+        invStr.append(to_string(articulos[i].cantidad));
+        invStr.append("\n");
+    }
+    return invStr;
+}
